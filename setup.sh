61914506a300649fe37db0133ab9135186910159
kriap139 <kripin41@gmail.com>
2024-01-07 00:12:45 +0100

    improved setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-config_dir=$HOME/.config/chezmoi
+data_dir="$HOME/.local/share/chezmoi"
+setup_gen="$data_dir/setup_gen.toml"
+config_dir="/home/$USER/.config/chezmoi"
 config="$config_dir/chezmoi.toml"
 
-mkdir -p $config_dir 
-touch "$config"
+rm -r "$config_dir" 2> /dev/null
+mkdir -p $config_dir
 
+touch "$setup_gen"
+touch "$config"
+echo -e "[keepassxc]\n\tdatabase='$1'" > "$setup_gen"
 echo -e "[keepassxc]\n\tdatabase='$1'" > "$config"
 
-chezmoi init https://github.com/kriap139/dotfiles.git
+if [ ! -d $data_dir ]; then 
+    chezmoi init https://github.com/kriap139/dotfiles.git
+    cd $data_dir
+    git remote set-url origin git@github.com:kriap139/dotfiles.git
+else
+    chezmoi init --apply
+fi
+
+rm -f $setup_gen
